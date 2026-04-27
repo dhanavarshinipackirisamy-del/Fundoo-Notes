716107dbefa27d453108ffb35d5f0a90dc3130aa
Dhanavarshini <dp9387@srmist.edu.in>
2026-04-27 12:46:56 +0530

    [dhana]: UC1: Initial Spring Boot setup with package structure and test controller

---
+++ src/main/java/com/bridgelabz/fundoonote/FundoonoteApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.fundoonote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundoonoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundoonoteApplication.class, args);
+	}
+
+}
